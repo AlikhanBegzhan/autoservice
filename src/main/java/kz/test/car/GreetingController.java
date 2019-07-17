5d81320a126a6a0ed73285ca2dff6276c95424bd
Alikhan <begjzhan@gmail.com>
2019-07-18 01:07:46 +0600

    Modified main code

--- src/main/java/kz/test/car/GreetingController.java
+++ src/main/java/kz/test/car/GreetingController.java
@@ -9,8 +9,10 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
+import java.util.ArrayList;
 import java.util.List;
 import  java.util.Map;
+import java.util.Optional;
 
 import static javassist.bytecode.StackMapTable.NULL;
 import static javassist.bytecode.StackMapTable.tag;
@@ -61,6 +63,62 @@ public class GreetingController {
         } else{
             cars = carRepo.findAll();
         }
+        model.put("cars", cars);
+
+        return "main";
+    }
+
+    @PostMapping("delete")
+    public String delete(@RequestParam String delete, Map<String, Object> model){
+        Iterable<Car> cars1;
+        List<Car> cars2;
+        List<Car> cars;
+        Car car;
+
+        cars1 = carRepo.findAll();
+        cars = (List<Car>)cars1;
+        cars1 = carRepo.findByCarOwnerName(delete);
+        cars2 = (List)cars1;
+        car = cars2.get(0);
+        cars.remove(car);
+        carRepo.delete(car);
+
+        cars1 = carRepo.findAll();
+        cars = (List<Car>)cars1;
+        model.put("cars", cars);
+
+        return "main";
+    }
+
+    @PostMapping("update")
+    public String update( @RequestParam Integer carId,
+                          @RequestParam String carModel, @RequestParam String carYear,
+                         @RequestParam String carMileage, @RequestParam String carOwnerName,
+                         @RequestParam String carOwnerNumber, @RequestParam String carOwnerAddress,
+                         @RequestParam String carMasterName, @RequestParam String carMasterWork,
+                         Map<String, Object> model){
+        Optional<Car> cars1;
+        Iterable<Car> cars;
+        Car car;
+
+        cars1 = carRepo.findById(carId);
+        car = cars1.get();
+
+        if(carModel != "") car.setCarModel(carModel);
+        if(carYear != "") car.setCarYear(carYear);
+        if(carMileage != ""){
+            int mileage = Integer.parseInt(carMileage);
+            car.setCarMileage(mileage);
+        }
+        if(carOwnerName != "") car.setCarOwnerName(carOwnerName);
+        if(carOwnerNumber != "") car.setCarOwnerNumber(carOwnerNumber);
+        if(carOwnerAddress != "") car.setCarOwnerAddress(carOwnerAddress);
+        if(carMasterName != "") car.setCarMasterName(carMasterName);
+        if(carMasterWork != "") car.setCarMasterWork(carMasterWork);
+
+        carRepo.save(car);
+
+        cars = carRepo.findAll();
         model.put("cars", cars);
 
         return "main";
